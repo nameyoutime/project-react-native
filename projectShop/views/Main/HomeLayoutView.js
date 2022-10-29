c835fd0c1808a080ff0e9cffa7f81648d75542ec
Thien <bananakind123@gmail.com>
2022-10-29 16:00:30 +0700

    bottom nav design

--- projectShop/views/Main/HomeLayoutView.js
+++ projectShop/views/Main/HomeLayoutView.js
@@ -5,6 +5,8 @@ import ShopLayoutView from '../shop/ShopLayoutView';
 import { useNavigation } from '@react-navigation/native';
 import CartLayoutView from '../shop/cart/CartLayoutView';
 import ProfileLayoutView from '../Profile/ProfileLayout';
+import Icon from 'react-native-vector-icons/FontAwesome'
+
 const Tab = createBottomTabNavigator();
 const HomeLayoutView = (props) => {
     const navigation = useNavigation();
@@ -15,40 +17,55 @@ const HomeLayoutView = (props) => {
     }, [])
 
     return (
-        <Tab.Navigator initialRouteName='Shop' >
+        <Tab.Navigator initialRouteName='Shop'
+            screenOptions={{
+                tabBarShowLabel: false, tabBarStyle: {
+                    // position:'absolute',
+
+                    backgroundColor: '#CECECE',
+                    borderTopColor: 'transparent',
+                    height: 60,
+                    borderTopLeftRadius: 15,
+                    borderTopRightRadius: 15,
+
+                }
+            }}
+
+        >
             <Tab.Screen name="Profile" component={ProfileLayoutView} options={
-
                 {
-                    headerTitle: (props) =>
-                        <>
-                            <Text onPress={() => { navigation.navigate('Profile') }}>
-                                Profile
-                            </Text>
-
-                        </>
+                    tabBarIcon: ({ focused }) => (
+                        <View onPress={() => { navigation.navigate('Profile') }}>
+                            {focused ?(<Icon  name='user' color='#40B5E7' size={40} />):(<Icon  name='user' color='grey' size={40} />)}
+                        </View>
+                    ),
                 }
             } />
             <Tab.Screen name="Shop" component={ShopLayoutView} options={
                 {
-                    headerTitle: (props) =>
-                        <>
-                            <Text onPress={() => { navigation.navigate('Main shop') }}>
-                                Shop
-                            </Text>
-
-                        </>
+                    tabBarIcon: ({ focused }) => (
+                        <View onPress={() => { navigation.navigate('Main shop') }}>
+                            {focused ?(<Icon  name='shopping-bag' color='#40B5E7' size={40} />):(<Icon  name='shopping-bag' color='grey' size={40} />)}
+                        </View>
+                    ),
+                    
                 }
 
             } />
             <Tab.Screen name="Cart" component={CartLayoutView} options={
                 {
-                    headerTitle: (props) =>
-                        <>
-                            <Text onPress={() => { navigation.navigate('Cart') }}>
-                                Cart
-                            </Text>
+                    tabBarIcon: ({ focused }) => (
+                        <View onPress={() => { navigation.navigate('Cart') }}>
+                            {focused ?(<Icon  name='shopping-cart' color='#40B5E7' size={40} />):(<Icon  name='shopping-cart' color='grey' size={40} />)}
+                        </View>
+                    ),
+                    // headerTitle: (props) =>
+                    //     <>
+                    //         <Text onPress={() => { navigation.navigate('Cart') }}>
+                    //             Cart
+                    //         </Text>
 
-                        </>
+                    //     </>
                 }
 
             } />
--- projectShop/views/Profile/OrderHistoryView.js
+++ projectShop/views/Profile/OrderHistoryView.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, TextInput, TouchableOpacity, Image,ScrollView } from 'react-native'
+import { View, Text, Button, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { useIsFocused } from '@react-navigation/native';
 import orderApi from '../../api/orderApi';
@@ -35,20 +35,25 @@ const OrderHistoryView = (props) => {
                 {orders.map((order) => {
                     return (
                         <View key={order._id} style={styles.card1}>
-                            
+
                             <Text style={styles.label}>Order's total price: <Text style={styles.labelInner}>{order.total}</Text></Text>
                             <Text style={styles.label}>Order's status: <Text style={styles.labelInner}>{order.status}</Text></Text>
                             <Text style={styles.label}>Products:</Text>
                             {handleRenderProducts(order.products)}
                             {/* if admin then have button */}
-                            <TouchableOpacity style={[styles.button, styles.button2]} onPress={() => handleGetUser(order.user.user)} >  
-                                <Text style={styles.btnTextblack}>View User</Text>
-                            </TouchableOpacity>
+                            {isAdmin && (
+                                <TouchableOpacity style={[styles.button, styles.button2]} onPress={() => handleGetUser(order.user.user)} >
+                                    <Text style={styles.btnTextblack}>View User</Text>
+                                </TouchableOpacity>
+
+                            )}
+
                             {/* render products in orders */}
-                            
-                            
+
+
                         </View>
-                    )})}
+                    )
+                })}
             </ScrollView>
 
         )
@@ -57,39 +62,41 @@ const OrderHistoryView = (props) => {
     const handleRenderProducts = (products) => {
         // console.log(products)
         return (
-            <View> 
-                {products.map((item, index) => {let product = item.product;
-                return (
-                    <View style={styles.card2} key={product._id}>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Image source={{ uri: product.images[0].url }} style={styles.cardImage} />
-                            <View style={{ paddingHorizontal: 10 }}> 
-                                <Text style={styles.label}>Title: <Text style={styles.labelInner}> {product.title}</Text></Text>
-                                <Text style={styles.label}>Price: <Text style={styles.labelInner}> {product.price}</Text></Text>
-                                <Text style={styles.label}>Order quantity: <Text style={styles.labelInner}> {item.quantity}</Text></Text>
+            <View>
+                {products.map((item, index) => {
+                    let product = item.product;
+                    return (
+                        <View style={styles.card2} key={product._id}>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Image source={{ uri: product.images[0].url }} style={styles.cardImage} />
+                                <View style={{ paddingHorizontal: 10 }}>
+                                    <Text style={styles.label}>Title: <Text style={styles.labelInner}> {product.title}</Text></Text>
+                                    <Text style={styles.label}>Price: <Text style={styles.labelInner}> {product.price}</Text></Text>
+                                    <Text style={styles.label}>Order quantity: <Text style={styles.labelInner}> {item.quantity}</Text></Text>
+                                </View>
                             </View>
+
+
+
                         </View>
-                        
-                        
-                        
-                    </View>
 
-                //     <TouchableOpacity style={style.card} onPress={() => handdleViewDetail(product)} >
-                //     <View style={{ display: 'flex', flexDirection: 'row' }}>
-                //         <Image source={{ uri: product.images[0].url }} style={style.cardImage} />
-                //         <View style={{ paddingHorizontal: 10 }}>
-                //             <Text style={style.label}>Title:<Text style={style.labelInner}> {product.title}</Text></Text>
-                //             <Text style={style.label}>Description:<Text style={style.labelInner}> {product.description}</Text></Text>
-                //             <Text style={style.label}>Price:<Text style={style.labelInner}> {product.price}</Text></Text>
+                        //     <TouchableOpacity style={style.card} onPress={() => handdleViewDetail(product)} >
+                        //     <View style={{ display: 'flex', flexDirection: 'row' }}>
+                        //         <Image source={{ uri: product.images[0].url }} style={style.cardImage} />
+                        //         <View style={{ paddingHorizontal: 10 }}>
+                        //             <Text style={style.label}>Title:<Text style={style.labelInner}> {product.title}</Text></Text>
+                        //             <Text style={style.label}>Description:<Text style={style.labelInner}> {product.description}</Text></Text>
+                        //             <Text style={style.label}>Price:<Text style={style.labelInner}> {product.price}</Text></Text>
 
-                //             <View style={{ display: 'flex', flexDirection: 'row' }}>
-                //                 <Text style={style.label}>Category: </Text>
-                //                 {product.categories.map((cate) => { return (<Text style={style.labelInner} key={cate._id}> {cate.title}</Text>) })}
-                //             </View>
-                //         </View>
-                //     </View>
-                //     </TouchableOpacity>
-                )})}
+                        //             <View style={{ display: 'flex', flexDirection: 'row' }}>
+                        //                 <Text style={style.label}>Category: </Text>
+                        //                 {product.categories.map((cate) => { return (<Text style={style.labelInner} key={cate._id}> {cate.title}</Text>) })}
+                        //             </View>
+                        //         </View>
+                        //     </View>
+                        //     </TouchableOpacity>
+                    )
+                })}
             </View>
         )
     }
